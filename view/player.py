501ab2d91f73b97a5c8e196a4a79b774bc229ffb
Shmuel_B <bitan.shmuelm@gmail.com>
2023-09-26 23:54:14 +0200

    update ajout creation toournoi retour des joueur et organisation des matchs

--- view/player.py
+++ view/player.py
@@ -1,10 +1,15 @@
 import re
+from models.player import Player
+
+
 def check_date_of_birth():
     dob = input("rentrez une date de naissance dans le format JJ-MM-AAAA ")
     if not re.match(r'[0-9]{2}-[0-9]{2}-[0-9]{4}', dob):
         print("une daste respectant le format JJ-MM-AAAA ")
         return check_date_of_birth()
     return dob
+
+
 def check_input():
     name = input("rentrez un nom ")
     if not name.isalpha():
@@ -12,17 +17,21 @@ def check_input():
         return check_input()
     return name
 
+
 def check_id():
     id = input(" rentrez l identifiant national d echec du joueur ")
-    if not re.match(r'[A-Z]{2}[0-9]{5}', id) :
+    if not re.match(r'[A-Z]{2}[0-9]{5}', id):
         check_id()
     return id
+
+
 def check_rank():
     rank = input("rentrez un rank ")
+    rank_int = int(rank)
     if not rank.isnumeric():
         print("Uniquement des chiffres et un rank valide  ")
         return check_rank()
-    if rank.isnumeric() and rank>3000:
+    if rank.isnumeric() and rank_int > 3000:
         print("Uniquement des chiffres et un rank valide  ")
         return check_rank()
 
@@ -33,7 +42,6 @@ def create_player_view():
     name = check_input()
     first_name = check_input()
 
-
     date_of_birth = check_date_of_birth()
 
     id = check_id()
@@ -41,12 +49,7 @@ def create_player_view():
     rank = check_rank()
 
     print(f"Joueur {first_name} {name} créé.")
+    player = Player(id, name, first_name, date_of_birth, rank)
+    return player
 
-    return {
-        "name": name,
-        "first_name": first_name,
-        "date_of_birth": date_of_birth,
-        "id": id,
-        "total_score": 0,
-        "rank": rank,
-    }
+
--- view/views.py
+++ view/views.py
@@ -1,55 +1,46 @@
-from models.player import *
-from ctrl.playermanager import *
-from models.tournament import Tournament
+from ctrl.playermanager import new_player, PlayerManager
+from ctrl.rapports import get_all_players_alphabetical
 from ctrl.tournament import TournamentManager
 
+player_manager = PlayerManager("players.json")
+tournament_manager = TournamentManager("tournament.json")
 
-class Menu:
-    def __init__(self):
-        self.player_manager = PlayerManager()
-        self.tournament_manager = TournamentManager()
 
-    def show_menu(self):
-        while True:
-            print("\nMain Menu:")
-            print("1. Create Player")
-            print("2. Modify Player")
-            print("3. Create Tournament")
-            print("4. Modify Tournament")
-            print("5. Exit")
-            choice = input("Enter your choice: ")
+def create_player():
+    new_player()
 
-            if choice == "1":
-                self.create_player()
-            elif choice == "2":
-                self.modify_player()
-            elif choice == "3":
-                self.create_tournament()
-            elif choice == "4":
-                self.modify_tournament()
-            elif choice == "5":
-                print("Exiting...")
-                break
-            else:
-                print("Invalid choice. Please choose again.")
 
-    def create_player(self):
-        print("\nCreate Player")
-        # Implement player creation logic here
+def see_all_players():
+    get_all_players_alphabetical()
 
-    def modify_player(self):
-        print("\nModify Player")
-        # Implement player modification logic here
 
-    def create_tournament(self):
-        print("\nCreate Tournament")
-        # Implement tournament creation logic here
 
-    def modify_tournament(self):
-        print("\nModify Tournament")
-        # Implement tournament modification logic here
+def create_tournament(self):
+ print("rien")
+
+def modify_tournament(self):
+    print("\nModify Tournament")
 
 
 if __name__ == "__main__":
-    menu = Menu()
-    menu.show_menu()
+    print("\nMain Menu:")
+    print("1. Create Player")
+    print("2. voir tout les joueurs ")
+    print("3. Create Tournament")
+    print("4. Modify Tournament")
+    print("5. Exit")
+    choice = input("Enter your choice: ")
+
+    if choice == "1":
+        new_player()
+    elif choice == "2":
+        get_all_players_alphabetical()
+    elif choice == "3":
+        create_tournament()
+    elif choice == "4":
+        modify_tournament()
+    elif choice == "5":
+        print("Exiting...")
+
+    else:
+        print("Invalid choice. Please choose again.")
